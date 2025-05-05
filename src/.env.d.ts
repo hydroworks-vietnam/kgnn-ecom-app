1f3aa161a46bc70fb10dffab1dd95b49496debf8
skyrocketeer <duylinh.huynh1995@gmail.com>
2025-05-05 16:51:07 +0700

    try to fix amplify build error

--- src/.env.d.ts
+++ src/.env.d.ts
@@ -5,3 +5,32 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare module 'astro:transitions' {
+  export const transition: any;
+  export const Transition: any;
+  export const ViewTransitions: any;
+  
+  export interface TransitionBeforePreparationEvent extends Event {
+    from: URL;
+    to: URL;
+    direction: 'forward' | 'back';
+  }
+  
+  export interface TransitionBeforeSwapEvent extends Event {
+    from: URL;
+    to: URL;
+    direction: 'forward' | 'back';
+  }
+  
+  export interface TransitionAfterSwapEvent extends Event {
+    from: URL;
+    to: URL;
+    direction: 'forward' | 'back';
+  }
+}
+
+declare module 'astro:transitions/client' {
+  export function navigate(href: string): Promise<void>;
+  export function preload(href: string): void;
+}
